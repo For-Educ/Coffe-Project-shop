7ff4202d7b2f422f8ddd85f1489c4142b0e09fd6
Ernest5621 <popovych.ernest@student.uzhnu.edu.ua>
2024-10-25 23:48:07 +0300

    initializing commit

---
+++ mainProjectExtra.js
@@ -0,0 +1,53 @@
+
+
+
+const fatherWindow = document.querySelector('.father-window');
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+   setTimeout(() => {
+     
+    document.body.style.opacity = '1';
+   }, 100)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// to see de facto winth of viewport 
+const infoWindow = document.createElement('div'); 
+document.body.appendChild(infoWindow);
+infoWindow.className = 'info-window';
+
+
+window.addEventListener('resize', (event) => {
+  const windowWidth = document.body.clientWidth;
+
+  infoWindow.textContent = `${windowWidth}`;
+
+
+   
+}
+);
+
