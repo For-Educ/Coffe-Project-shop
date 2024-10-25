9948ff0549b58b8c234cee6cdfe54bf7f44607d8
Ernest5621 <popovych.ernest@student.uzhnu.edu.ua>
2024-10-26 00:08:22 +0300

    version 1.2

--- mainProjectExtra.js
+++ mainProjectExtra.js
@@ -1,18 +1,42 @@
-const fatherWindow = document.querySelector('.father-window');
 document.addEventListener('DOMContentLoaded', () => {
-   setTimeout(() => {
+  setTimeout(() => {
     document.body.style.opacity = '1';
-   }, 100)
-})
-// to see de facto winth of viewport 
-const infoWindow = document.createElement('div'); 
+  }, 100);
+  //added cool card switching!
+  const ImageList = document.querySelectorAll('section div .image-expectation img');
+  let isExtended = false;
+  document.querySelector('.image-expectation').addEventListener('click', (event) => {
+    event.preventDefault();
+    ImageList[0].style.transform = 'translateX(60%)';
+    ImageList[1].style.transform = 'translateX(-60%)';
+    if (!isExtended) {
+      ImageList[1].style.zIndex = '100';
+      setTimeout(() => {
+        ImageList[0].style.zIndex = '1000';
+        ImageList[0].style.transform = 'translateX(0%) scale(1)';
+        ImageList[1].style.transform = 'translateX(0%) scale(0.6)';
+      }, 450);
+      isExtended = true;
+    } else {
+      ImageList[0].style.zIndex = '100';
+      setTimeout(() => {
+        ImageList[1].style.zIndex = '1000';
+        ImageList[0].style.transform = 'translateX(0%) scale(0.6)';
+        // ImageList[0].style.transform = 'translateX(0%)';
+        ImageList[1].style.transform = 'translateX(0%) scale(1)';
+      }, 450);
+      isExtended = false;
+    }
+  });
+});
+// to see de facto width of viewport
+const infoWindow = document.createElement('div');
 document.body.appendChild(infoWindow);
 infoWindow.className = 'info-window';
-window.addEventListener('resize', (event) => {
+window.addEventListener('resize', () => {
   const windowWidth = document.body.clientWidth;
   infoWindow.textContent = `${windowWidth}`;
-}
-);
+});
 
 
 
