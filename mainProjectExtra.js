ea213285ca044dfa515bb67933da3cfd4f5a3d16
Ernest5621 <popovych.ernest@student.uzhnu.edu.ua>
2024-10-26 00:01:01 +0300

    version 1.1

--- mainProjectExtra.js
+++ mainProjectExtra.js
@@ -1,22 +1,18 @@
-
-
-
 const fatherWindow = document.querySelector('.father-window');
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
-
-
    setTimeout(() => {
-     
     document.body.style.opacity = '1';
    }, 100)
 })
-
-
+// to see de facto winth of viewport 
+const infoWindow = document.createElement('div'); 
+document.body.appendChild(infoWindow);
+infoWindow.className = 'info-window';
+window.addEventListener('resize', (event) => {
+  const windowWidth = document.body.clientWidth;
+  infoWindow.textContent = `${windowWidth}`;
+}
+);
 
 
 
